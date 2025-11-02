9035c08ae5f0a6d9fbcf0f60d2933d875cc54365
PeterLi817 <plpeter817@gmail.com>
2025-11-02 17:44:26 -0500

    add databases

--- app.py
+++ app.py
@@ -1,8 +1,25 @@
 from flask import Flask
 from views import main
+from models import db, User
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+
 app.register_blueprint(main)
 
+
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()  # Create database tables
+
+        # Add fake admin user if it doesn't exist
+        if not User.query.filter_by(email='admin@colby.edu').first():
+            admin = User(email='admin@colby.edu', role='admin')
+            admin.set_password('1234')
+            db.session.add(admin)
+            db.session.commit()
+
     app.run(debug=True)
