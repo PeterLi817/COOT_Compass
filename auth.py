18c3c84f472c6206aefe99dc9a3a889ac03172f0
shelby roman <shelbyroman@shelbys-MacBook-Pro-162.local>
2025-11-04 15:26:15 -0500

    Implement login features

---
+++ auth.py
@@ -0,0 +1,27 @@
+from flask import Blueprint, render_template, redirect, url_for, flash
+from flask import request
+from models import db, User
+from flask_login import login_user, login_required, logout_user
+
+auth_blueprint = Blueprint('auth', __name__)
+
+@auth_blueprint.route('/login', methods=['GET', 'POST'])
+def login():
+  if request.method == 'POST':
+      email = request.form['email']
+      password = request.form['password']
+      user = User.query.filter_by(email=email).first()
+      print(email, password)
+      if user and user.check_password(password):
+          login_user(user)
+          return redirect(url_for('main.groups'))
+      else:
+          flash('Invalid email or password. Please try again.')
+          return redirect(url_for('auth.login'))
+  return render_template('login.html')
+
+# @auth_blueprint.route('/logout')
+# @login_required
+# def logout():
+#     logout_user()
+#     return redirect(url_for('auth.login'))
--- views.py
+++ views.py
@@ -1,32 +1,35 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 from models import Student, Trip
+from flask_login import login_required
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
+@login_required
 def index():
-    trips = Trip.query.all()
-    return render_template('groups.html', trips=trips)
+    # trips = Trip.query.all()
+    # return render_template('login.html', trips=trips)
+    return redirect(url_for('main.groups'))
 
 @main.route('/trips')
+@login_required
 def trips():
     trips = Trip.query.all()
     return render_template('trips.html', trips=trips)
 
 @main.route('/first-years')
+@login_required
 def first_years():
     students = Student.query.all()
     return render_template('first-years.html', students=students)
 
 @main.route('/groups')
+@login_required
 def groups():
     trips = Trip.query.all()
     return render_template('groups.html', trips=trips)
 
 @main.route('/settings')
+@login_required
 def settings():
     return render_template('settings.html')
-
-@main.route('/login')
-def login():
-    return render_template('login.html')
